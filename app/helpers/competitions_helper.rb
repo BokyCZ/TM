3d0cc02ca3a1ab4bc833f14331b7a6d7bc4197dc
Bokuvka Lukas <lukas.bokuvka@topmonks.com>
2012-07-04 00:36:07 +0200

    Update comparison (competitions) + added standings for table competitions

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -30,50 +30,42 @@ module CompetitionsHelper
   end
 
   def color_and_comparison_first(drawOne, drawTwo)
-    if drawOne.first_round < drawTwo.first_round
-      0
-    else
-      1
-    end
+    color_and_comparison(drawOne.first_round, drawTwo.first_round, drawOne.player.qualification.standings, drawTwo.player.qualification.standings)
   end
 
   def color_and_comparison_second(drawOne, drawTwo)
-    if drawOne.second_round < drawTwo.second_round
-      0
-    else
-      1
-    end
+    color_and_comparison(drawOne.second_round, drawTwo.second_round, drawOne.player.qualification.standings, drawTwo.player.qualification.standings)
   end
 
   def color_and_comparison_third(drawOne, drawTwo)
-    if drawOne.third_round < drawTwo.third_round
-      0
-    else
-      1
-    end
+    color_and_comparison(drawOne.third_round, drawTwo.third_round, drawOne.player.qualification.standings, drawTwo.player.qualification.standings)
   end
 
   def color_and_comparison_quarterfinal(drawOne, drawTwo)
-    if drawOne.quarterfinal_round < drawTwo.quarterfinal_round
-      0
-    else
-      1
-    end
+    color_and_comparison(drawOne.quarterfinal_round, drawTwo.quarterfinal_round, drawOne.player.qualification.standings, drawTwo.player.qualification.standings)
   end
 
   def color_and_comparison_semifinal(drawOne, drawTwo)
-    if drawOne.semifinal_round < drawTwo.semifinal_round
-      0
-    else
-      1
-    end
+    color_and_comparison(drawOne.semifinal_round, drawTwo.semifinal_round, drawOne.player.qualification.standings, drawTwo.player.qualification.standings)
   end
 
   def color_and_comparison_final(drawOne, drawTwo)
-    if drawOne.final_round < drawTwo.final_round
+    color_and_comparison(drawOne.final_round, drawTwo.final_round, drawOne.player.qualification.standings, drawTwo.player.qualification.standings)
+  end
+
+  def color_and_comparison(drawOne, drawTwo, totalOne, totalTwo)
+    if drawOne < drawTwo
       0
-    else
-      1
+      else
+        if drawOne > drawTwo
+          1
+        else
+          if totalOne < totalTwo
+            1
+          else
+            0
+          end
+        end
     end
   end
 
